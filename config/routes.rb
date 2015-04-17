13d9a735f25d4e628751ca64d64f478e5dc9a63a
Won Wook SONG <wsong0512@gmail.com>
2015-04-17 22:23:28 +0900

    rake routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,18 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#angular'
+  root to: 'application#angular'
+
+  resources :posts, only: [:create, :index, :show] do
+      resources :comments, only: [:show, :create] do
+          member do
+              put '/upvote' => 'comments#upvote'
+          end
+      end
+      member do
+          put '/upvote' => 'posts#upvote'
+      end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
